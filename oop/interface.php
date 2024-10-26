1eacc30b1f9aba0076020dba07061da938ee1773
LepharamRamchiary <lepharamchiary@gmail.com>
2024-10-26 12:38:02 +0530

    interface

---
+++ oop/interface.php
@@ -0,0 +1,43 @@
+<!-- interface same as abstract class, but we use multiple interface in a one class or child class -->
+
+<?php
+
+interface inter1{
+    public function sum();
+    public function sub();
+}
+
+interface inter2 {
+    public function mul();
+    public function div();
+}
+
+class MyChild implements inter1, inter2{
+    function sum(){
+     return 10+10;   
+    }
+
+    function sub(){
+        return 10-5;
+    }
+
+    function mul(){
+     return 10*10;   
+    }
+
+    function div(){
+     return 10/2;   
+    }
+}
+
+$obj = new MyChild();
+echo $obj->sum();
+echo "<br>";
+echo $obj->sub();
+echo "<br>";
+echo $obj->mul();
+echo "<br>";
+echo $obj->div();
+
+
+?>
