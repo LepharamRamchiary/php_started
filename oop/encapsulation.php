72688f6ae2af642faa35823957ccd1a5af7a8a8e
LepharamRamchiary <lepharamchiary@gmail.com>
2024-10-26 12:04:20 +0530

    encapsulation

---
+++ oop/encapsulation.php
@@ -0,0 +1,38 @@
+<?php
+// public
+// protected
+// private
+
+class MyClass{
+    // public $name = "Lepharam";
+
+    // function __construct()
+    // {
+    //     echo $this->name;
+    // }
+
+
+    // Note- protected are inharited with child class
+    // protected $name = "Lepharam";
+    // function printName(){
+    //     $this->name;
+    // }
+
+    // function setName(){
+    //     $this->name = "Lepharam Ramchiary";
+    // }
+    // function getName(){
+    //     return $this->name;
+    // }
+
+    // Note- Private are not inharited with child class
+}
+
+// $obj = new MyClass();
+
+$obj = new MyClass();
+// echo $obj->printName();
+// echo $obj->setName();
+// echo $obj->getName();
+// echo $obj->setName();
+?>
